43832642f07ee1d2f056861078c0361e8b47512f
hayato94087 <hayato94087@gmail.com>
2024-05-31 05:27:15 +0900

    言語モデルを利用

---
+++ demo01.ts
@@ -0,0 +1,20 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import 'dotenv/config'
+
+// model
+const model = new ChatOpenAI({model: "gpt-3.5-turbo"});
+
+// system
+const system =  `あなたは入力された文章を英語に変換するアシスタントです`;
+
+// messages
+const messages = [
+  new SystemMessage(system),
+  new HumanMessage("昔々あるところにおじいさんとおばあさんがいました"),
+];
+
+// result
+const modelResult = await model.invoke(messages);
+console.log(modelResult);

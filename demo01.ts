3847a799781817d9ee28cfe404bdbe45d069343a
hayato94087 <hayato94087@gmail.com>
2024-05-31 05:30:36 +0900

    言語モデルを利用

--- demo01.ts
+++ demo01.ts
@@ -1,6 +1,5 @@
 import { ChatOpenAI } from "@langchain/openai";
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
-import { StringOutputParser } from "@langchain/core/output_parsers";
 import 'dotenv/config'
 
 // model
